2d618d7300f56ad197e21582ca106ec108987476
mins00kwon <mskwon0918@gmail.com>
2025-02-03 09:50:38 +0900

    2025 2월 3일 1교시 수업 내용

--- 02_java/chap06-class-and-object-lecture-source/src/com/ohgiraffers/section03/abstraction/Application.java
+++ 02_java/chap06-class-and-object-lecture-source/src/com/ohgiraffers/section03/abstraction/Application.java
@@ -2,6 +2,20 @@ package com.ohgiraffers.section03.abstraction;
 
 import java.util.Scanner;
 
+/* 설명:
+ *   추상화란?
+ *       공통된 부분을 추출하고 공통되지 않고 불필요한 부분을 제거한다는 의미를 가지며
+ *       추상황의 목적은 유연성을 확보하기 위함이다.
+ *       유연성 확보는 여러 곳에 적용될 수 있는 유연한 객체를 의미하며,
+ *       재사용성이 높아질 수 있게 한다는 의미이다
+ *       (우리는 자연스럽게 추상화를 이루었지만, 그렇게 추상화된 클래스로 유연한 객체들을 생성할 수 있다.)*/
+
+/* 설명:
+ *   1. 협력
+ *   2. 책임
+ *   3. 역할
+ * */
+
 public class Application {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);

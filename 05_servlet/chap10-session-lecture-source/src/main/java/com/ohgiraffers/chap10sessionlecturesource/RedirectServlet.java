d94ac32de7a8b3b29d9bbb0b551ae5a2367338f5
mins00kwon <mskwon0918@gmail.com>
2025-02-25 15:24:56 +0900

    2025.02.25.6교시 Session

---
+++ 05_servlet/chap10-session-lecture-source/src/main/java/com/ohgiraffers/chap10sessionlecturesource/RedirectServlet.java
@@ -0,0 +1,22 @@
+package com.ohgiraffers.chap10sessionlecturesource;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/redirect")
+public class RedirectServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        String firstName = session.getAttribute("firstName").toString();
+        String lastName = (String) session.getAttribute("lastName");
+
+        System.out.println("firstName = " + firstName);
+        System.out.println("lastName = " + lastName);
+    }
+}

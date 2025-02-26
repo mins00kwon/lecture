d9157f88a5ccb8c927e77ba20577dfb1503385a8
mins00kwon <mskwon0918@gmail.com>
2025-02-26 11:25:20 +0900

    2025.02.26.3교시 BCrypt encoding 및 matches

--- 05_servlet/chap11--filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/PasswordEncryptFilter.java
+++ 05_servlet/chap11--filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/PasswordEncryptFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @WebFilter("/member/*")
@@ -14,5 +15,7 @@ public class PasswordEncryptFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
         /* 설명: 기존의 Request 객체를 우리가 getParameter()를 재정의한 Request 객체로 교체한다*/
+        RequestWrapper wrapper=new RequestWrapper((HttpServletRequest) servletRequest);
+        filterChain.doFilter(wrapper, servletResponse);
     }
 }
--- 05_servlet/chap11--filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/RequestWrapper.java
+++ 05_servlet/chap11--filter-lecture-source/src/main/java/com/ohgiraffers/section02/uses/RequestWrapper.java
@@ -1,6 +1,26 @@
 package com.ohgiraffers.section02.uses;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletRequestWrapper;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class RequestWrapper extends HttpServletRequestWrapper {
+
+    public RequestWrapper(HttpServletRequest request) {
+        super(request);
+    }
+
+    @Override
+    public String getParameter(String key) {
+        /* 설명: 'password' 라는 키 값으로 getParameter 메소드 사용 시에는 암호화된 값을 반환하도록 재정의*/
+        String value = "";
+        if("password".equals(key)) {// 키 값으로 "password"가 넘어올 때
+            System.out.println("패스워드 꺼낼 시");
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();        /* 필기: 암호화 인코더 객체 생성 */
+            value=passwordEncoder.encode(super.getParameter(key));
+            System.out.println("암호화된 패스워드: "+value);
+            return value;           // 키 값이 "password"가 아닐 때
+        }
+        return super.getParameter(key);
+    }
 }

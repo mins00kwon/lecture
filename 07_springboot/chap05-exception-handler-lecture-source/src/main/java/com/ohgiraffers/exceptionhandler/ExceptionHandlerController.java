f7b9dc10f1460094cfbbd655d8129f418c7ef1f6
mins00kwon <mskwon0918@gmail.com>
2025-03-06 14:24:25 +0900

    사용자 정의형 예외 처리 방법

--- 07_springboot/chap05-exception-handler-lecture-source/src/main/java/com/ohgiraffers/exceptionhandler/ExceptionHandlerController.java
+++ 07_springboot/chap05-exception-handler-lecture-source/src/main/java/com/ohgiraffers/exceptionhandler/ExceptionHandlerController.java
@@ -1,5 +1,6 @@
 package com.ohgiraffers.exceptionhandler;
 
+import jdk.jshell.spi.ExecutionControl.UserException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,14 @@ public class ExceptionHandlerController {
     @GetMapping("simple-null")
     public String simpleNullPointerExceptionTest() {
         if(true) throw new NullPointerException();
+        return "/";
+    }
+    @GetMapping("simple-user")
+    public String userExceptionTest() throws MemberRegistException {
+
+        if(true) {
+            throw new MemberRegistException("당신은 안돼요!");
+        }
         return "/";
     }
 }
--- 07_springboot/chap05-exception-handler-lecture-source/src/main/java/com/ohgiraffers/exceptionhandler/RootConfiguration.java
+++ 07_springboot/chap05-exception-handler-lecture-source/src/main/java/com/ohgiraffers/exceptionhandler/RootConfiguration.java
@@ -13,6 +13,7 @@ public class RootConfiguration {
         /* 설명: 예외 타입과 처리할 페이지 설정*/
         Properties props = new Properties();
         props.setProperty("java.lang.NullPointerException", "error/nullPointer");
+        props.setProperty("com.ohgiraffers.exceptionhandler.MemberRegistException","error/memberRegist");
 
          /*설명: 전체 예외 관련되어 설정하기*/
          /*설명: 1. 예외에 따른 페이지 설정한 것 활용 */
@@ -23,6 +24,8 @@ public class RootConfiguration {
 
         /* 설명: 3. 예외 객체를 담기 위한 Attribute key 값 설정 (화면에서 예외 메시지 확인 시 사용할 key 값)*/
         exceptionResolver.setExceptionAttribute("exceptionMessage");
+
+
 
         return exceptionResolver;
     }

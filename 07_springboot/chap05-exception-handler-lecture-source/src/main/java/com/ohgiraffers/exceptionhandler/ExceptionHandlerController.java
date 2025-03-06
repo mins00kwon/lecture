f929e00e3fc412bf08f7430668d57034ecf0ed9a
mins00kwon <mskwon0918@gmail.com>
2025-03-06 14:36:35 +0900

    클래스 단위 익셉션 핸들링 @ExceptionHandler 활용 파라미터로 Exception.class 전달

--- 07_springboot/chap05-exception-handler-lecture-source/src/main/java/com/ohgiraffers/exceptionhandler/ExceptionHandlerController.java
+++ 07_springboot/chap05-exception-handler-lecture-source/src/main/java/com/ohgiraffers/exceptionhandler/ExceptionHandlerController.java
@@ -3,6 +3,7 @@ package com.ohgiraffers.exceptionhandler;
 import jdk.jshell.spi.ExecutionControl.UserException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,5 +22,33 @@ public class ExceptionHandlerController {
             throw new MemberRegistException("당신은 안돼요!");
         }
         return "/";
+    }
+
+    @GetMapping("annotation-null")
+    public String nullPointerExceptionHandlerTest() {
+        String str=null;
+        str.charAt(0);
+
+        return "/";
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public String nullPointerExceptionHandler() {
+        System.out.println("이 Controller에서 NullPointer 예외 발생 시 여기 오는지 확인");
+        return "error/nullPointer";
+    }
+    @GetMapping("annotation-user")
+    public String userExceptionHandlerTest() throws MemberRegistException {
+        if(true) {
+            throw new MemberRegistException("응애!! 응애!!");
+        }
+
+        return "/";
+    }
+
+    @ExceptionHandler(MemberRegistException.class)
+    public String userExceptionHandler() {
+        System.out.println("이 Controller에서 UserException 예외 발생 시 여기 오는지 확인");
+        return "error/default";
     }
 }

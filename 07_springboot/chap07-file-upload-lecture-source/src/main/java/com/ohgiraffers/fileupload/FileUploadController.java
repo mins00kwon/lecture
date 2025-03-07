9886f883b86ffabed346d648eadcbb94d067bfec
mins00kwon <mskwon0918@gmail.com>
2025-03-07 14:50:59 +0900

    멀티 파일 업로드 전체적인 흐름은 비슷하지만 1. 리스트에 담아야 하고 2. 완료되지 않은 트랜잭션에 대해 롤백을 해야하면 3. 프론트에서 받아주는 thymeleaf 문법을 알아야 한다.

--- 07_springboot/chap07-file-upload-lecture-source/src/main/java/com/ohgiraffers/fileupload/FileUploadController.java
+++ 07_springboot/chap07-file-upload-lecture-source/src/main/java/com/ohgiraffers/fileupload/FileUploadController.java
@@ -2,7 +2,9 @@ package com.ohgiraffers.fileupload;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import org.springframework.stereotype.Controller;
@@ -57,6 +59,7 @@ public class FileUploadController {
 
             rttr.addFlashAttribute("message", originFileName+" 업로드 성공!");
             rttr.addFlashAttribute("img","/img/single/"+savedName);
+            System.out.println("/img/single/"+savedName);
             rttr.addFlashAttribute("singleFileDescription", singleFileDescription);
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -67,4 +70,51 @@ public class FileUploadController {
 
     @GetMapping("result")
     public void result() {}
+
+    @PostMapping("multi-file")
+    public String multiFile(@RequestParam List<MultipartFile> multiFiles,
+                            @RequestParam String multiFileDescription,
+                            RedirectAttributes rttr) {
+
+        String filePath="/Users/mk/uploadFiles";
+
+        /* 필기: DB로 보낼 파일 담을 리스트 */
+        List<Map<String,String>> files=new ArrayList<>();
+        /* 설명: 화면에서 각 파일마다 img 태그의 src 속성으로 적용하기 위한 문자열을 담은 컬렉션*/
+        List<String> imgSrcs=new ArrayList<>();
+
+
+        try {
+            for (int i = 0; i < multiFiles.size(); i++) {
+                /* 설명: 각 파일마다 리네임 */
+                String originalFileName = multiFiles.get(i).getOriginalFilename();
+                String ext = originalFileName.substring(originalFileName.lastIndexOf("."));
+                String savedName = UUID.randomUUID().toString().replace("-", "") + ext;
+
+                /* 설명: 파일 저장 경로에 저장*/
+                multiFiles.get(i).transferTo(new File(filePath + "/img/multi/" + savedName));
+
+                /* 설명: DB에 보낼 값 설정(Map<String, String>)*/
+                Map<String, String> file = new HashMap<>();
+                file.put("originalFileName", originalFileName);
+                file.put("savedName", savedName);
+                file.put("filePath", "img/multi");
+                file.put("multiFileDescription", multiFileDescription);
+                files.add(file);    // 크기를 지정해놓지 않았으니 가능
+                imgSrcs.add("/img/multi/"+savedName);
+            }//for 문 종료. try 문 내부
+            /* 설명: 여기까지 성공했다면 파일 저장 및 DB inert까지 모두 완성되었으니 화면의 재료 작성 */
+            rttr.addFlashAttribute("message", "다중 파일 업로드 성공!");
+            rttr.addFlashAttribute("imgs", imgSrcs);
+            rttr.addFlashAttribute("multiFileDescription", multiFileDescription);
+        } catch (IOException e) {
+            /* 설명: transferTo 파일을 경로에 저장하다가 예외가 발생하면 앞에 이미 성공해서 저장된 파일들을 찾아 지움*/
+            for (int i = 0; i < files.size(); i++) {
+                Map<String, String> file = files.get(i);
+                new File(filePath+"/img/multi/"+file.get("savedName")).delete();
+            }
+            rttr.addFlashAttribute("message", "파일 업로드 실패");
+        }
+        return "redirect:/result";
+    }
 }

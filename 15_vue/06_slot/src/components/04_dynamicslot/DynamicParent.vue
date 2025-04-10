2b3cebb6b9d7e2446e0addd28f604cec464cae9d
mins00kwon <mskwon0918@gmail.com>
2025-04-10 10:16:20 +0900

    ch06. slot

--- 15_vue/06_slot/src/components/04_dynamicslot/DynamicParent.vue
+++ 15_vue/06_slot/src/components/04_dynamicslot/DynamicParent.vue
@@ -4,6 +4,7 @@
         <input type="text" v-model="dynamicName">
         <DynamicChild>
             <!-- #은 v-slot 을 대체한다는 것을 기억하자. -->
+            <!-- #[]는 동적 태그 이름을 사용할 때 활용한다 -->
             <template #[dynamicName]>
                 {{ dynamicName }}
             </template>
